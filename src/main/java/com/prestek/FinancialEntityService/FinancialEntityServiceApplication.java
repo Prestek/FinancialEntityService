33d44f2873a9fd55410aaf288ae0f78904afb1d6
JAGBytes <jorge.gamboa-s@mail.escuelaing.edu.co>
2025-11-01 22:42:41 -0500

    creating proyect from spring initilizr

---
+++ src/main/java/com/prestek/FinancialEntityService/FinancialEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prestek.FinancialEntityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialEntityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialEntityServiceApplication.class, args);
+	}
+
+}
